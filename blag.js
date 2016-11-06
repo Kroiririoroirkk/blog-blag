fa6eaf70c163fbdf55ed7a1e030b94c252584a59
Kroiririoroirkk <taoeric9@gmail.com>
2016-11-06 17:04:10 -0500

    Update blag.js

--- blag.js
+++ blag.js
@@ -1,1 +1,11 @@
-
+$(document).ready(function() {
+  random = $("#random");
+  background = random.css("background-color");
+  random.css("cursor","pointer").click(function() {
+    window.location.href = "https://kroiririoroirkk.github.io/blog-blag/#";
+  }).hover(function() {
+    random.css("background-color", 0x222288);
+  }, function() {
+    random.css("background-color", background);
+  });
+});
