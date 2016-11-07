e90be02cb9c6d4477b5b8dd1eeb629cbe05e3c3b
Kroiririoroirkk <taoeric9@gmail.com>
2016-11-06 20:17:19 -0500

    Update blag.js

--- blag.js
+++ blag.js
@@ -8,4 +8,28 @@ $(document).ready(function() {
   }, function() {
     random.css("background-color", background);
   });
+  var $contents = $("#contents");
+  var blogXml = $("#blogxml").text();
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(blogXml, "text/xml");
+  var comics = doc.getElementsByTagNameNS("kroicomics", "comic");
+  comics.forEach(function(comic){
+    /* <div class="comicContainer" id=number>
+         <div class="cTitle">title</div>
+         <div class="comic">
+           <img src="imagePath" alt=title>
+         </div>
+         Permalink to comic: https://kroiririoroirkk.github.io/blog-blag/# + number
+         Image URL: https://kroiririoroirkk.github.io/blog-blag/ + imagePath
+       </div>
+    */
+    var number = comic.attr("number");
+    var title = comic.attr("title");
+    var comicLink = comic.attr("comic");
+    var element = '<div class="comicContainer" id="' + number + '"><div class="cTitle">' + title + '</div>' +
+        '<div class="comic"> <img src="' + comicLink + '" alt="' + title + '"> </div>' + 
+        'Permalink to comic: https://kroiririoroirkk.github.io/blog-blag/#' + number + '<br>' +
+        'Image URL: https://kroiririoroirkk.github.io/blog-blag/' + comicLink;
+    contents.prepend(element);
+  });
 });
